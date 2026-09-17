9242b49cccf060a91791e1ae2ad972345a847dac
Khuong <killer123578910@gmail.com>
2026-09-18 02:04:19 +0700

    formated

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,9 +22,11 @@ return {
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
 
-        -- Manual format keybind instead (async to avoid blocking)
+        -- Manual format keybind with external formatter (async to avoid blocking)
         vim.keymap.set("n", "<leader>fl", function()
-          vim.lsp.buf.format({ async = true })
+          -- Use google-java-format instead of LSP
+          vim.cmd("silent !google-java-format -i %")
+          vim.cmd("e!") -- Reload file after formatting
         end, { noremap = true, buffer = bufnr })
       end,
       -- Reduce JDTLS resource consumption
@@ -53,10 +55,52 @@ return {
       },
     })
 
+    require("lspconfig").pylsp.setup({
+      handlers = {
+        ["$/progress"] = function(_, result, ctx) end,
+      },
+      -- Disable aggressive formatting on save
+      on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+
+        -- Manual format keybind with external formatters (async to avoid blocking)
+        vim.keymap.set("n", "<leader>fl", function()
+          -- Use external formatters instead of LSP
+          vim.cmd("silent !black % && isort %")
+          vim.cmd("e!") -- Reload file after formatting
+        end, { noremap = true, buffer = bufnr })
+      end,
+      -- Reduce pylsp resource consumption and disable expensive plugins
+      settings = {
+        pylsp = {
+          -- Disable all formatters from running on save
+          formatCommand = {},
+          plugins = {
+            -- Disable expensive linters/formatters that thrash disk
+            pycodestyle = { enabled = false },
+            pydocstyle = { enabled = false },
+            autopep8 = { enabled = false },
+            yapf = { enabled = false },
+            black = { enabled = false }, -- Don't format on save
+            isort = { enabled = false }, -- Don't sort imports on save
+            pylint = { enabled = false }, -- Disable pylint (slow)
+            flake8 = { enabled = false }, -- Disable flake8 (slow)
+            mccabe = { enabled = false }, -- Disable McCabe complexity checker
+            -- Keep these enabled but lightweight
+            pyflakes = { enabled = true }, -- Fast syntax checking
+            rope = { enabled = false }, -- Disable rope (can be slow)
+            -- Optionally use fast linters only
+            pyls_mypy = { enabled = false }, -- mypy is slow, disable
+          },
+        },
+      },
+    })
+
     -- Disable format-on-save autocmd to prevent SSD thrashing
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("DisableLspFormatOnSave", { clear = true }),
-      pattern = "*.java",
+      pattern = { "*.java", "*.py" },
       callback = function()
         -- Format only on manual command, not on every save
       end,
